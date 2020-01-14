e81e451abe3ceff035fa69bfc8ec8b0ab15e4795
lauren brett <laurencareybrett@gmail.com>
2020-01-14 15:49:05 +0000

    mvp

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+   
+    new Vue ({
+        el: "#app",
+        data: {
+            countries: [],
+            selectedCountry: null
+        },
+        mounted: function(){
+            this.fetchCountries()
+        },
+        methods: {
+            fetchCountries: function () {
+                fetch("https://restcountries.eu/rest/v2/all")
+                    .then(response => response.json())
+                    .then(countries =>  this.countries = countries);
+            }
+        }
+    })
+    
+})
